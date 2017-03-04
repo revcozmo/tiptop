f8fc596e4929ac1cb307e2cf85262abff88ddc2a
Jonas Eichhorn <jonas@eichhorn-networks.de>
2017-03-04 14:02:12 +0100

    Added factory for generating random levels; not functional yet

---
+++ www/js/factory/generator.js
@@ -0,0 +1,10 @@
+// Generates random gameTable objects, colorLists and amount of clicks you need to solve the level
+  app.factory('generator', function() {
+
+    // INTERFACE
+      var interface = {
+
+      };
+
+      return interface;
+  })
