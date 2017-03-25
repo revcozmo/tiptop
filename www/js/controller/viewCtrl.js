c2281e65ef7c3fea48435b7b4eba6ac8e3be2f0e
Jonas Eichhorn <jonas@eichhorn-networks.de>
2017-03-25 23:30:02 +0100

    Added Popover menu

--- www/js/controller/viewCtrl.js
+++ www/js/controller/viewCtrl.js
@@ -1,5 +1,5 @@
 // Controls the $scope not belonging to the game behaviour
-  app.controller('viewCtrl', function($scope, $rootScope, $ionicSlideBoxDelegate, $ionicSideMenuDelegate, cache, view, tool) {
+  app.controller('viewCtrl', function($scope, $rootScope, $ionicSlideBoxDelegate, $ionicSideMenuDelegate, $ionicPopover, cache, view, tool) {
     // Shows/Hides the settings and help screens
       $scope.showScreen = function(screen) {
           switch(screen) {
@@ -16,6 +16,32 @@
 
           cache.setScreen(screen);
       };
+
+    // Menu Popover
+      $ionicPopover.fromTemplateUrl('../../templates/menuPopover.html', {
+        scope: $scope
+      }).then(function(popover) {
+        $scope.popover = popover;
+      });
+
+      $scope.openPopover = function($event) {
+        $scope.popover.show($event);
+      };
+      $scope.closePopover = function() {
+        $scope.popover.hide();
+      };
+      // Cleanup the popover when we're done with it!
+        $scope.$on('$destroy', function() {
+          $scope.popover.remove();
+        });
+      // Execute action on hidden popover
+        $scope.$on('popover.hidden', function() {
+          // Execute action
+        });
+      // Execute action on remove popover
+        $scope.$on('popover.removed', function() {
+          // Execute action
+        });
 
     // Sets new language
       $scope.langChange = function(newLang) {console.log("Test");
