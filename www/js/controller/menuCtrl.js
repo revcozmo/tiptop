72df6cdc3acf9de0a3c93855632592ee0015e618
Jonas Eichhorn <jonas@eichhorn-networks.de>
2017-04-11 17:52:25 +0200

    Added modal for implementation of ranking list; shifted popup control from viewCtrl to menuCtrl

---
+++ www/js/controller/menuCtrl.js
@@ -0,0 +1,47 @@
+// Controls the $scope belonging to the popover menu
+  app.controller('menuCtrl', function($scope, $ionicModal, $ionicPopup, cache) {
+    // Ranking Modal
+      $ionicModal.fromTemplateUrl('templates/rankingModal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+
+      $scope.openModal = function() {
+        $scope.modal.show();
+      };
+      $scope.closeModal = function() {
+        $scope.modal.remove();
+      };
+      // Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function() {
+          $scope.modal.remove();
+        });
+      // Execute action on hide modal
+        $scope.$on('modal.hidden', function() {
+          // Execute action
+        });
+      // Execute action on remove modal
+        $scope.$on('modal.removed', function() {
+          // Execute action
+        });
+
+    // Username popup; Username will be used for the global ranking
+      $scope.enterName = function() {
+        var namePopup = cache.getTranslation().namePopup;
+        $ionicPopup.prompt({
+          title: namePopup[0],
+          template: namePopup[1],
+          inputType: 'text',
+          inputPlaceholder: namePopup[2]
+        }).then(function(res) {
+
+          if (res == undefined || res == "")
+            return;
+
+          alert("Name: " + res);
+
+        });
+      }
+  })
--- www/js/controller/viewCtrl.js
+++ www/js/controller/viewCtrl.js
@@ -1,5 +1,5 @@
 // Controls the $scope not belonging to the game behaviour
-  app.controller('viewCtrl', function($scope, $rootScope, $ionicSlideBoxDelegate, $ionicSideMenuDelegate, $ionicPopup, $ionicPopover, cache, view, tool) {
+  app.controller('viewCtrl', function($scope, $rootScope, $ionicSlideBoxDelegate, $ionicSideMenuDelegate, $ionicPopup, $ionicPopover, $ionicModal, cache, view, tool) {
     // Shows/Hides the settings and help screens
       $scope.showScreen = function(screen) {
           switch(screen) {
@@ -42,27 +42,6 @@
         $scope.$on('popover.removed', function() {
           // Execute action
         });
-
-    // Username popup; Username will be used for the global ranking
-      $scope.enterName = function() {
-        var namePopup = cache.getTranslation().namePopup;
-        $ionicPopup.prompt({
-
-          title: namePopup[0],
-          template: namePopup[1],
-          inputType: 'text',
-          inputPlaceholder: namePopup[2]
-
-        }).then(function(res) {
-
-          if (res == undefined || res == "")
-            return;
-
-          alert("Name: " + res);
-
-        });
-      }
-
 
     // Sets new language
       $scope.langChange = function(newLang) {console.log("Test");
